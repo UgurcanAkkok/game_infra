82b8889f03e80dd2c35101ec8169becc36dff754
Uğurcan Akkök <akkokugurcan@gmail.com>
2023-11-05 22:06:50 +0300

    add jenkinsfile

--- source/build.sh
+++ source/build.sh
@@ -13,6 +13,8 @@ FULL_IMAGE_NAME="${REPOSITORY}/${IMAGE}:${VERSION}-c${COMMIT_ID}"
 # You need to be in the source directory, which has the Dockerfile
 docker build -t $FULL_IMAGE_NAME .
 
+echo "$FULL_IMAGE_NAME" > .full_image_name
 echo "------------------------------"
 echo "The image is ready: $FULL_IMAGE_NAME"
 echo "------------------------------"
+echo "Pushing the image: $FULL_IMAGE_NAME"
